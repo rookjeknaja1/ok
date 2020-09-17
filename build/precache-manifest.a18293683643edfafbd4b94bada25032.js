eb0700ba0df390fd596d8238edec28e6bd083e44
rookjeknaja1 <dsitsy@database.asia-southeast1-b.c.my-project-print-289803.internal>
2020-09-17 03:42:02 +0000

    thisok

---
+++ build/precache-manifest.a18293683643edfafbd4b94bada25032.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f5ddda69b4ad23ffd3000f622e02581",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ad92508957c7e789a95",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "345574b346125dec79d1",
+    "url": "/static/css/main.acce490e.chunk.css"
+  },
+  {
+    "revision": "0ad92508957c7e789a95",
+    "url": "/static/js/2.27b4ed2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.27b4ed2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345574b346125dec79d1",
+    "url": "/static/js/main.9e03f9dc.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
